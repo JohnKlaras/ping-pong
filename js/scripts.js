16d06882ea52d02cdecb9594ccdf8c71d2aa6e01
Rick Klaras <jrklaras@gmail.com>
2015-10-17 12:29:27 -0700

    create div for match results, add.append method to li

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,35 @@ $(document).ready(function() {
 
     $("#blank form").submit(function(event) {
       var howManyMatches = $("input#matches").val();
-      console.log(howManyMatches);
 
       for (var index = 1; index <= howManyMatches; index += 1) {
-        // console.log(index);
-      if (index % 3 === 0 && index % 5 === 0) {
-        document.write("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
-      } else if (index % 3 === 0) {
-        document.write("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
-      } else if (index % 5 === 0) {
-        document.write("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
-      } else {
-        document.write("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
-      }
-      event.preventDefault();
 
+      // if (index % 3 === 0 && index % 5 === 0) {
+      //   document.write("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
+      // } else if (index % 3 === 0) {
+      //   document.write("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
+      // } else if (index % 5 === 0) {
+      //   document.write("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
+      // } else {
+      //   document.write("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
+        // let's try this instead
+      if (index % 3 === 0 && index % 5 === 0) {
+        $("ul.whoWon").append("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
+      } else if (index % 3 === 0) {
+        $("ul.whoWon").append("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
+      } else if (index % 5 === 0) {
+        $("ul.whoWon").append("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
+      } else {
+        $("ul.whoWon").append("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
+
+      };
+
+
+      event.preventDefault();
+      $(".results").show();
 
     };
-    // $('li').prepend('Round' + index + ":");
+
   });
 
 });
