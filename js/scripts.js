309cefe7c5be0455a838c2a8f5b79414d70de9aa
Rick Klaras <jrklaras@gmail.com>
2015-10-17 15:20:32 -0700

    add styling to text, fix js, add pics, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,20 @@ $(document).ready(function() {
 
       for (var index = 1; index <= howManyMatches; index += 1) {
 
-      // if (index % 3 === 0 && index % 5 === 0) {
-      //   document.write("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
-      // } else if (index % 3 === 0) {
-      //   document.write("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
-      // } else if (index % 5 === 0) {
-      //   document.write("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
-      // } else {
-      //   document.write("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
-        // let's try this instead
-      if (index % 3 === 0 && index % 5 === 0) {
-        $("ul.whoWon").append("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
-      } else if (index % 3 === 0) {
-        $("ul.whoWon").append("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
-      } else if (index % 5 === 0) {
-        $("ul.whoWon").append("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
-      } else {
-        $("ul.whoWon").append("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
-
-      };
+        if (index % 3 === 0 && index % 5 === 0) {
+          $("ul.whoWon").append("<li>Round " + index + ": Ping-pong! Bruce Lee won!</li>");
+        } else if (index % 3 === 0) {
+          $("ul.whoWon").append("<li>Round " + index + ": Ping! Bruce Lee won!</li>");
+        } else if (index % 5 === 0) {
+          $("ul.whoWon").append("<li>Round " + index + ": Pong! Bruce Lee won!</li>");
+        } else {
+          $("ul.whoWon").append("<li>Round " + index + ": You lasted " + index + " volleys against Bruce Lee, but he still won.</li>");
+        };
 
 
       event.preventDefault();
-      $(".results").show();
+      $("#intro").hide();
+      $("#results").show();
 
     };
 
